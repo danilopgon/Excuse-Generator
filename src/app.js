562184bc2e5b46188af0209682c177fc43e02cf8
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-05-02 18:42:39 +0000

    Finished excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const randomNumber = () => {
+    const random = Math.floor(Math.random() * 4);
+    return random;
+  };
+
+  const excuseGenerator = () => {
+    let who = ["My cat", "My cousin", "Your raccoon", "The president"];
+    let action = ["ate", "shot at", "crushed", "smashed"];
+    let what = [
+      "my homework",
+      "my computer",
+      "my dreams",
+      "the concept of time"
+    ];
+    let when = [
+      "before lunch",
+      "right on time",
+      "during my sabbatical year",
+      "while I was taking a nap"
+    ];
+
+    return `${who[randomNumber()]} ${action[randomNumber()]} ${
+      what[randomNumber()]
+    } ${when[randomNumber()]}`;
+  };
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
